17a536d639d0c86ec3bbc89152de4d7b1b071b2e
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-07-02 19:59:20 +0200

    sparar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Switch, Route, useHistory } from 'react-router-dom'
 
 import SpeciesList from './SpeciesList'
+import SelectedSpecies from './SelectedSpecies'
 
 export const App = () => {
+  const [species, setSpecies] = useState([])
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.REACT_APP_API_KEY
+      }
+    }
+    fetch('https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/biotopes?taxa=100067,100013,100763,1079,221913,245630', options)
+      .then((res) => res.json())
+      .then((json) => setSpecies(json))
+      .catch(error => console.error(error))
+
+  }, []);
+  console.log(setSpecies)
   return (
-    <div>
+    <div className="app-container">
       <h1>Upplands landskapsarter och deras biotoper</h1>
-      <SpeciesList />
+      <SpeciesList species={species} />
+      <SelectedSpecies species={species} />
     </div>
   )
 }
--- src/SelectedSpecies.js
+++ src/SelectedSpecies.js
@@ -1,23 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const SpeciesList = () => {
-  const [species, setSpecies] = useState([])
-
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Ocp-Apim-Subscription-Key': process.env.REACT_APP_API_KEY
-      }
-    }
-    fetch('https://api.artdatabanken.se/information/v1/speciesdataservice/v1/speciesdata/biotopes?taxa=100067,100013,100763,1079,221913,245630', options)
-      .then((res) => res.json())
-      .then((json) => setSpecies(json))
-      .catch(error => console.error(error))
-
-  }, []);
-  console.log(setSpecies)
-
+const SelectedSpecies = ({ species }) => {
   return (
     <div className="card-container">
       {species.map((item) => {
@@ -41,6 +24,5 @@ const SpeciesList = () => {
     </div>
   )
 }
-export default SpeciesList
 
-//eslint-disable-next-line react-hooks/exhaustive-deps
+export default SelectedSpecies
