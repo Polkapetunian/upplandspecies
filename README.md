c9e40a6bf304be575c914648be4be9cc2bc1e3ce
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-07-12 23:41:39 +0200

    filtered biotopes on important biotopes

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ This app shows the "landscape" species of Uppland. The species information is fe
 
 Den här appen visar Upplands landskapsarter. Informationen om arterna hämtas från ArtDatabankens API "Species Information". 
 
+## API endpoints 
+
+GET /speciesdata/biotopes
+
+Användes för att hämta biotopinformation om arterna. (Filtrerades för att bara visa de biotoper som är viktiga för arten.)
+
+This endpoint was used to fetch biotope information of the species. (The data was filtered so that only the important biotopes are displayed.)
+
 
--- src/components/SelectedSpecies.js
+++ src/components/SelectedSpecies.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
 const SelectedSpecies = ({ selectedSpecies }) => {
+
   return (
     selectedSpecies &&
     <div className="card" key={selectedSpecies.swedishName}>
       <h2>{selectedSpecies.swedishName}</h2>
       <h3 className="latin-name">{selectedSpecies.scientificName}</h3>
-      <h3>Biotoper</h3>
-      {selectedSpecies.speciesData.map((item) => {
+      <h3>Viktiga biotoper</h3>
+      {selectedSpecies.speciesData.filter(item => item.significance === "Viktig").map((item) => {
         return (
           <div key={item.id}>
             <ul className="biotope-list" >
-              <li>{item.name} <span>{item.significance}</span></li>
+              <li>{item.name}</li>
             </ul>
           </div>
         )
