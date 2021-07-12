4eb0a0f75af3106ad4185f435ee812cb69880b78
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-07-12 23:09:15 +0200

    implemented clicl outside menu to close menu

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
-# Technigo React Starter App
+# Upplands landskapsarter
 
-This app has been generated using `create-react-app`, then cleaned up a little so we have a project with just the elements we need to get started.
+This app shows the "landscape" species of Uppland. The species information is fetched from the Species Information API from the SLU Swedish Species Information Centre.
 
-Add components in the `src` folder and use them in `src/app.js` to get started.
+Den här appen visar Upplands landskapsarter. Informationen om arterna hämtas från ArtDatabankens API "Species Information". 
 
-## Usage
 
-1. On GitHub, click the 'use this template' button to create a new repo on your account
-1. Clone the new repo to your computer
-1. Install required dependencies with npm: `npm install`
-1. Start the project: `npm start`
-
-Alternatively, if you don't want a new git repo on your account;
-
-1. Download & extract the [latest release](https://github.com/Technigo/react-starter/releases/latest) from GitHub
-1. Open the directory in the terminal: `cd /path/to/react-starter`
-1. Install required dependencies with npm: `npm install`
-1. Start the project: `npm start`
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Switch, Route, useHistory } from 'react-router-dom'
+import React, { useState, useEffect, useRef } from 'react'
 
 import SpeciesList from './components/SpeciesList'
 import SelectedSpecies from './components/SelectedSpecies'
 import Burger from './components/Burger'
+import { useOnClickOutside } from './hooks'
 
 export const App = () => {
   const [species, setSpecies] = useState([])
   const [selectedSpeciesId, setSelectedSpeciesId] = useState(undefined)
   const [open, setOpen] = useState(false)
+
+  const node = useRef()
+  useOnClickOutside(node, () => setOpen(false))
 
   useEffect(() => {
     const options = {
@@ -23,21 +26,18 @@ export const App = () => {
       .catch(error => console.error(error))
 
   }, []);
-  console.log(species)
-  console.log(selectedSpeciesId)
-  console.log(selectedSpecies)
 
   const selectedSpecies = species.find(item => item.taxonId === selectedSpeciesId)
 
   return (
     <div className="app-container">
-      <h1>Upplands landskapsarter och deras biotoper</h1>
-      <div className="main-container">
+      <h1 className="top-header">Upplands landskapsarter</h1>
+      <div className="nav-container" ref={node}>
         <Burger open={open} setOpen={setOpen} />
         <SpeciesList species={species} setSelectedSpeciesId={setSelectedSpeciesId} open={open} setOpen={setOpen} />
-        {selectedSpecies &&
-          <SelectedSpecies species={species} selectedSpecies={selectedSpecies} />}
       </div>
+      {selectedSpecies &&
+        <SelectedSpecies species={species} selectedSpecies={selectedSpecies} />}
     </div>
   )
 }
