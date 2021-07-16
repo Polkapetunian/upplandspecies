ea24b9b28ccef790fbd9ef11689bf5cd38356a8e
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-07-16 18:33:34 +0200

    changed typo

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,7 +4,7 @@ const Introduction = () => {
   return (
     <div className="card">
       <h2>Vad är det här?</h2>
-      <p>Det här är en Single Page Application som visar information om Upplands landskapsarter. Informationen hämtas från ArtDatabankens API <span className="inline-title">Species information</span>.</p>
+      <p>Det här är en Single Page Application som visar information om Upplands landskapsarter. Informationen hämtas från Artdatabankens API <span className="inline-title">Species information</span>.</p>
       <p>API står för application programming interface (applikationsprogrammeringsgränssnitt).
         Genom ett API kan man dela med sig av sina data på ett kontrollerat sätt.
         Användarna får inte direkt tillgång till databasen,
